954b3a19d7930b70331cb3095524b286a4dc3da0
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-03-02 13:58:30 +0100

    rewrite meta

--- functions/f.download_table_make.R
+++ functions/f.download_table_make.R
@@ -8,27 +8,63 @@
 
 
 
-f.download_table_make <- function(){
+f.download_table_make <- function(sleep.min = 0,
+                                  sleep.max = 1,
+                                  debug = FALSE,
+                                  debug.pages = 20){
+
+    offset.url <- f.linkextract("https://www.bundesfinanzhof.de/de/entscheidungen/entscheidungen-online/")
+    offset.url <- grep("search-form", offset.url, value = TRUE)
+    
+    offset.raw <- gsub(".*Boffset%5D=([0-9]+)#search-form.*", "\\1", offset.url)        
+    offset.max <- max(as.integer(offset.raw))
+    offset.all <- seq(0, offset.max, 10)
 
 
-    html <- rvest::read_html("https://www.bundesfinanzhof.de/de/entscheidungen/entscheidungen-online/")
+    if(debug == TRUE){
+
+    offset.all <- sort(sample(offset.all, debug.pages))
+        
+    }
+    
+    
+    url.all <- paste0("https://www.bundesfinanzhof.de/de/entscheidungen/entscheidungen-online/?tx_eossearch_eossearch[offset]=",
+                      offset.all,
+                      "#search-form")
 
     
-https://www.bundesfinanzhof.de/de/entscheidungen/entscheidungen-online/?tx_eossearch_eossearch[offset]=0#search-form
+    result.list <- lapply(url.all, f.extract_meta_bfh)
+    result.dt <- rbindlist(result.list)
+
+
 
 
     
+    release <- as.Date(release, format = "%d.%m.%Y")
 
+    datum <- as.Date(datum, format = "%d.%m.%Y")
+
+    bfh_id <- gsub(".*\\/(STR.*)\\/", "\\1", url_html)
+
+
+
+    
 }
 
 
 
-f.extract_meta_bfh <- function(html){
 
 
+f.extract_meta_bfh <- function(url,
+                               sleep.min = 0,
+                               sleep.max = 1,
+                               verbose = FALSE){
+
+    html <- rvest::read_html(url)
+    
     release <- rvest::html_nodes(html, "[data-label='Veröffentlichung am']")
     release <- rvest::html_text(release, trim = TRUE)
-    release <- as.Date(release, format = "%d.%m.%Y")
+
 
     slg <- rvest::html_nodes(html, "[data-label='V/NV']")
     slg <- rvest::html_text(slg, trim = TRUE)
@@ -38,6 +74,7 @@ f.extract_meta_bfh <- function(html){
 
     datum <- rvest::html_nodes(html, "[data-label='Entscheidung am']")
     datum <- rvest::html_text(datum, trim = TRUE)
+
 
     az <- rvest::html_nodes(html, "[data-label='Aktenzeichen']")
     az <- rvest::html_text(az, trim = TRUE)
@@ -58,6 +95,15 @@ f.extract_meta_bfh <- function(html){
                                         az = az,
                                         titel = titel,
                                         url_html = url_html)
+
+    if(verbose == TRUE){
+        
+        message(paste("Page", url, "completed."))
+
+    }
+
+    
+    Sys.sleep(runif(1, sleep.min, sleep.max))
 
     return(dt.return)
 
