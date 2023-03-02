00545327fc64fe7a5980d3b5c9fb5b1de9022b09
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-03-02 15:01:51 +0100

    move clean to finalize fun

--- functions/f.download_table_make.R
+++ functions/f.download_table_make.R
@@ -41,16 +41,6 @@ f.download_table_make <- function(sleep.min = 0.5,
     dt.return <- rbindlist(result.list)
 
 
-    
-
-    ## Clean Results
-    
-    dt.return$release <- as.Date(dt.return$release, format = "%d.%m.%Y")
-
-    dt.return$datum <- as.Date(dt.return$datum, format = "%d.%m.%Y")
-
-    dt.return$bfh_id <- gsub(".*\\/(STR.*)\\/", "\\1", dt.return$url_html)
-
 
 
     return(dt.return)
