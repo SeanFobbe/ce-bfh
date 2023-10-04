692fcbcd53299f34868c21e9127cb2dcc1dcb06a
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-10-04 20:26:59 +0200

    add debugging code

--- functions/f.download_manifest_finalize.R
+++ functions/f.download_manifest_finalize.R
@@ -1,12 +1,16 @@
-
+#' f.download_manifest_finalize
+#'
+#' Cleans up and tests the download manifest for the Bundesfinanzhof (BFH) database.
+#'
+#' @param x Data.table. A download manifest created by the f.download_manifest_make function.
+#'
+#' @return Data.table. The finalized download manifest.
 
 
 
 
 f.download_manifest_finalize <- function(x){
 
-
-        
 
     ## Clean Results
     
@@ -18,4 +22,27 @@ f.download_manifest_finalize <- function(x){
 
 
 
-    }
+    ## Tests
+    test_that("Class is correct.", {
+        expect_s3_class(dt.final, "data.table")
+    })
+   
+    test_that("BFH IDs are unique.", {
+        expect_equal(uniqueN(bfh_id),  x[,.N])
+    })
+
+    test_that("Dates are in ISO format.", {
+        expect_equal(uniqueN(bfh_id),  x[,.N])
+    })
+
+    
+
+    return(dt.return)
+
+
+}
+
+
+## DEBUGGING CODE
+
+# x <- tar_read(dt.download.manifest.raw)
