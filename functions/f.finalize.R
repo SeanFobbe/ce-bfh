25f3cbc26f3f879f798f429d9ea6e752e302d9ef
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-10-08 23:54:35 +0200

    add debugging code; more code to final

--- functions/f.finalize.R
+++ functions/f.finalize.R
@@ -4,10 +4,24 @@
 
 
 
-f.finalize <- function(dt.intermediate){
+f.finalize <- function(dt.intermediate,
+                       vars.additional,
+                       varnames){
 
 
-        ## Create var "doc_id"
+    ## Unit Test
+    test_that("Arguments have correct classes", {
+        expect_s3_class(x, "data.table")
+        expect_s3_class(vars.additional, "data.table")
+        expect_type(varnames, "character")
+    })
+
+    ## Bind additional vars
+    dt <- cbind(dt.intermediate,
+                vars.additional)
+    
+    
+    ## Create var "doc_id"
     dt$doc_id <- paste0("BFH_",
                         dt$slg,
                         "_",
@@ -24,11 +38,18 @@ f.finalize <- function(dt.intermediate){
                         dt$bfh_id)
 
 
+    ## Unit Test: Check variables and set column order
+    
+    varnames <- gsub("\\\\", "", varnames) # Remove LaTeX escape characters
+    data.table::setcolorder(dt.final, varnames)
+
+
+    
     ## Tests
     test_that("Class is correct.", {
         expect_s3_class(dt.final, "data.table")
     })
-   
+    
     test_that("BFH IDs are unique.", {
         expect_equal(sum(duplicated(dt.final$bf_id)),  0)
     })
@@ -36,6 +57,23 @@ f.finalize <- function(dt.intermediate){
     test_that("Doc IDs are unique.", {
         expect_equal(sum(duplicated(dt.final$doc_id)),  0)
     })
+
+
+    
+
+    return(dt.final)
     
     
-    }
+}
+
+
+
+## DEBUGGING CODE
+
+## library(data.table)
+## library(testthat)
+## tar_load(dt.intermediate)
+## vars.additional  <- tar_read(vars_additional)
+
+## variables.codebook <- fread("data/CE-BFH_Variables.csv")
+## varnames  <-  variables.codebook$varname
