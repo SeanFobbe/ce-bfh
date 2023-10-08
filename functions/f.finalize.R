92efe4a5a16442c2a3cd31ededc27a6bd77c4b6a
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-10-09 00:15:01 +0200

    add var creations to f.finalize

--- functions/f.finalize.R
+++ functions/f.finalize.R
@@ -20,25 +20,37 @@ f.finalize <- function(dt.intermediate,
     dt.final <- cbind(dt.intermediate,
                       vars.additional)
 
-    ## Create var "gericht"
+    ## Create var "entscheidungsjahr"
+    dt$entscheidungsjahr <- year(dt$datum)
 
+    ## Create var "veroeffentlichungsjahr"
+    dt$veroeffentlichungsjahr <- year(dt$veroeffentlichung)
+
+    ## Create var "pkh"
+    dt$pkh <- grepl("PKH", dt$aktenzeichen, ignore.case = TRUE)
+
+    ## Create var "adv"
+    dt$adv <- grepl("AdV", dt$aktenzeichen, ignore.case = TRUE)
+
+    
+    ## Create var "gericht"
     dt.final$gericht <- "BFH"
     
     ## Create var "doc_id"
     dt.final$doc_id <- paste0("BFH_",
-                        dt$bfhe,
-                        "_",
-                        dt$datum,
-                        "_",
-                        dt$spruchkoerper_az,
-                        "_",
-                        dt$registerzeichen,
-                        "_",
-                        dt$eingangsnummer,
-                        "_",
-                        dt$eingangsjahr_az,
-                        "_",
-                        dt$bfh_id)
+                              dt$bfhe,
+                              "_",
+                              dt$datum,
+                              "_",
+                              dt$spruchkoerper_az,
+                              "_",
+                              dt$registerzeichen,
+                              "_",
+                              dt$eingangsnummer,
+                              "_",
+                              dt$eingangsjahr_az,
+                              "_",
+                              dt$bfh_id)
 
 setdiff(varnames, names(dt.final))
     
@@ -63,6 +75,30 @@ setdiff(varnames, names(dt.final))
         expect_equal(sum(duplicated(dt.final$doc_id)),  0)
     })
 
+    test_that("Dates are in ISO format.", {
+        expect_true(all(grepl("[0-9]{4}-[0-9]{2}-[0-9]{2}", dt.final$veroeffentlichung)))
+        expect_true(all(grepl("[0-9]{4}-[0-9]{2}-[0-9]{2}", dt.final$datum)))
+    })
+    
+
+    test_that("var entscheidungsjahr contains only expected values.", {
+        expect_true(all(dt.final$entscheidungsjahr >= 2010))
+        expect_true(all(dt.final$entscheidungsjahr <= year(Sys.Date())))    
+    })
+
+    test_that("var veroeffentlichungsjahr contains only expected values.", {
+        expect_true(all(dt.final$entscheidungsjahr >= 2010))
+        expect_true(all(dt.final$entscheidungsjahr <= year(Sys.Date())))    
+    })
+    
+    test_that("var pkh contains only expected values.", {
+      expect_type(dt.final$adv, "logical")   
+    })
+    
+    test_that("var adv contains only expected values.", {
+      expect_type(dt.final$adv, "logical")   
+    })
+    
 
     
 
